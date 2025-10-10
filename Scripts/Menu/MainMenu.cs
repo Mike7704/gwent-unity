e00d0c49fbab95b29c6a267941c94405f620b0bc
Michael <82280788+Mike7704@users.noreply.github.com>
2025-10-10 11:24:28 +0100

    Added deck menu scene

--- Scripts/Menu/MainMenu.cs
+++ Scripts/Menu/MainMenu.cs
@@ -6,15 +6,15 @@ public class MainMenu : MonoBehaviour
     // Called when Play button is clicked
     public void PlayGame()
     {
-        Debug.Log("Game");
+        Debug.Log("Play Game");
         SceneManager.LoadScene("GameScene");
     }
 
     // Called when Deck button is clicked
     public void DeckMenu()
     {
-        Debug.Log("Deck");
-        SceneManager.LoadScene("DeckScene");
+        Debug.Log("Deck Menu");
+        SceneManager.LoadScene("DeckMenu");
     }
 
     // Called when Quit button is clicked
